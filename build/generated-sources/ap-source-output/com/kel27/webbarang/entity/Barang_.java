6c6e1d1f3214887ad1f8efe624041bd98bb8cbfe
novendraino <38321060+novendraino@users.noreply.github.com>
2020-06-12 15:17:14 +0700

    Add files via upload

---
+++ build/generated-sources/ap-source-output/com/kel27/webbarang/entity/Barang_.java
@@ -0,0 +1,17 @@
+package com.kel27.webbarang.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-06-12T15:12:43")
+@StaticMetamodel(Barang.class)
+public class Barang_ { 
+
+    public static volatile SingularAttribute<Barang, String> nama;
+    public static volatile SingularAttribute<Barang, String> harga;
+    public static volatile SingularAttribute<Barang, String> kategori;
+    public static volatile SingularAttribute<Barang, String> id;
+    public static volatile SingularAttribute<Barang, String> stok;
+
+}
